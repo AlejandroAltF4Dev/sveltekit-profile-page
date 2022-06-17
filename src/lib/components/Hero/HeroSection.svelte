3999b5e5550f8846fb883214e2fde4be654f2bed
Alejandro Alvarez <alex.david.alvarez.sosa@gmail.com>
2022-06-16 23:10:36 -0400

    Updated Hero section

--- src/lib/components/Hero/HeroSection.svelte
+++ src/lib/components/Hero/HeroSection.svelte
@@ -12,7 +12,7 @@
         <div class="flex flex-col">
             <h1 class="hero-text font-bold text-center text-[32px]  md:text-[44px]">Hello, my name is <span
                     class="font-bold text-primary">Alejandro</span></h1>
-            <p class="hero-text text-center text-[20px] mt-4">{hero.properties.Description.title[1].plain_text}</p>
+            <p class="hero-text text-center text-[20px] mt-4">{hero.properties?.Description?.title[0]?.plain_text}</p>
             <Button size="large" isLink type="primary" href={hero.properties.CV.files[0].file.url}
                     class="self-center text-[20px] font-medium mt-4 flex items-center">
                 <div class="w-8 h-8 mr-2">
